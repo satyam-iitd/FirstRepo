b5916ae7625e9de96e02d1f58b94477fdadec6c0
SatyamModi <smodi50448@gmail.com>
2022-06-12 23:02:09 +0530

    Revert "Added some changes to test.cpp"
    This reverts commit d3a06e490cdfe5881d9177da2b6e59655f8e9cb4.

--- test.cpp
+++ test.cpp
@@ -1,12 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+for (int i = 0; i < 5; i++)
 {
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "Satyam"
-             << "\n";
-    }
-    return 0;
+    cout << "Satyam" << "\n";
 }
