d3a06e490cdfe5881d9177da2b6e59655f8e9cb4
SatyamModi <smodi50448@gmail.com>
2022-06-12 21:27:54 +0530

    Added some changes to test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-for (int i = 0; i < 5; i++)
+int main()
 {
-    cout << "Satyam" << "\n";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Satyam"
+             << "\n";
+    }
+    return 0;
 }
