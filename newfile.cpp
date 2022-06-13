e5ef61f36faf0c0d99c1cf5f99dfe706075f9730
SatyamModi <smodi50448@gmail.com>
2022-06-13 09:02:40 +0530

    Added some code to newfile.cpp

---
+++ newfile.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Satyam" << "\n";
+    cout << "Hey, How is ur day" << "\n";
+    cout << "Hey, I am Satyam";
+    return 0;
+}
